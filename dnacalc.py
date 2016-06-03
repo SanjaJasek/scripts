93e031678c35bb136fca41fa758f575e10152ad0
Sanja Jasek <jasek.sanja@gmail.com>
2016-06-03 11:46:29 +0200

    dnacalc.py also now calculates base percentages

--- dnacalc.py
+++ dnacalc.py
@@ -5,5 +5,16 @@ DNASeq = 'ATGAAC'
 #print 'Sequence', DNASeq # -> only python 2
 print ('Sequence ' + DNASeq ) # -> python 2 and 3
 
-SeqLength = len(DNASeq)
-print(SeqLength)
+SeqLength = float(len(DNASeq))
+print( 'Length is ' + str(SeqLength))
+
+
+NumberA = DNASeq.count('A')
+NumberT = DNASeq.count('T')
+NumberG = DNASeq.count('G')
+NumberC = DNASeq.count('C')
+
+print('A: ' + str( NumberA / SeqLength * 100 ) +'%')
+print('T: ' + str( NumberT / SeqLength * 100 ) +'%')
+print('G: ' + str( NumberG / SeqLength * 100 ) +'%')
+print('C: ' + str( NumberC / SeqLength * 100 ) +'%')
