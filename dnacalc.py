ac0368fd3ad42c28e6845046c793bcb0c54e553d
Sanja Jasek <jasek.sanja@gmail.com>
2016-06-06 11:09:37 +0200

    added melt temp

--- dnacalc.py
+++ dnacalc.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
-DNASeq = 'ATGAAC'
+#DNASeq = 'ATGAAC'
+DNASeq = raw_input("Enter a DNA sequence:\n")
+DNASeq = DNASeq.upper()
 
 #print 'Sequence', DNASeq # -> only python 2
 print ('Sequence ' + DNASeq ) # -> python 2 and 3
@@ -14,7 +16,20 @@ NumberT = DNASeq.count('T')
 NumberG = DNASeq.count('G')
 NumberC = DNASeq.count('C')
 
-print('A: ' + str( NumberA / SeqLength * 100 ) +'%')
-print('T: ' + str( NumberT / SeqLength * 100 ) +'%')
-print('G: ' + str( NumberG / SeqLength * 100 ) +'%')
-print('C: ' + str( NumberC / SeqLength * 100 ) +'%')
+print('A: {:.2f}'.format(NumberA / SeqLength))
+print('T: {:.2f}'.format(NumberA / SeqLength))
+print('G: {:.2f}'.format(NumberA / SeqLength))
+print('C: {:.2f}'.format(NumberA / SeqLength))
+
+TotalStrong = NumberC + NumberG
+TotalWeak = NumberA + NumberT
+
+
+if SeqLength <=14:
+	MeltTemp = ( 4 * TotalStrong ) + ( 2 * TotalWeak )
+	print("Using short formula.")
+else:
+	MeltTemp = 64.9 + 42 * (TotalStrong - 16.4) / SeqLength
+	print("Using short formula.")
+
+print( "Melting Temp: {}".format(MeltTemp))
